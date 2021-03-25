ca33aa06f3de53294b9acf3d7b9c9273b96c1c9a
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-03-25 12:53:57 +0200

    create session for anonymous user

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 
+from store.models import Product
 
 
 class Basket():
@@ -10,32 +12,91 @@ class Basket():
         self.session = request.session
         
         if 'basket' not in request.session:
-            basket = self.session['basket'] = {'basket_quantity': 0}
+            basket = self.session['basket'] = {}
+            self.session['basket_quantity'] = 0
+            self.session['basket_price'] = 0
         else:
             basket = self.session['basket']
 
         self.basket = basket
-    
 
+    
     def add(self, product, product_quantity):
         """
         Add and update user's basket session data
         """
 
         product_id = str(product.id)
+        product_price = product.price * product_quantity
+
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price), 'quantity': int(product_quantity)}
+            # add new basket info to session data
+            self.basket[product_id] = {
+                'total_price': str(product_price),
+                'quantity': int(product_quantity)
+            }
         else:
+            # update session basket data
+            total_price = Decimal(self.basket[product_id]['total_price'])
+            total_price += product_price
+
+            self.basket[product_id]['total_price'] = str(total_price)
             self.basket[product_id]['quantity'] += product_quantity
 
-        self.basket['basket_quantity'] += product_quantity
+        self.session['basket_quantity'] += product_quantity
+        self.session['basket_price'] += float(product_price)
 
+        self.save()
+    
+
+    def delete(self, product):
+        """
+        Delete item from basket session data
+        """
+
+        product_id = str(product.id)
+        if product_id in self.basket:
+            item = self.basket[product_id]
+           
+            self.session['basket_quantity'] -= item['quantity']
+            self.session['basket_price'] -= float(item['total_price'])
+
+            del self.basket[product_id]
+
+        self.save()
+
+
+    def save(self):
         self.session.modified = True
 
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to query the db and return the products detail
+        """
+        product_ids = self.basket.keys()
+        products_in_basket = Product.active_products.filter(pk__in=product_ids) 
+        
+        # copy the session data in to a new dict to save up memory for the session data
+        basket = self.basket.copy()
 
-    def __len__(self):
+        for product in products_in_basket:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            yield item
+        
+    @property
+    def get_basket_price(self):
+        """
+        Get the basket total price
+        """
+        return format(self.session['basket_price'], '.2f')
+        
+
+    @property
+    def get_basket_quantity(self):
         """
         Get the basket data and count the total quantity
         """
-        return self.basket['basket_quantity']
+        return self.session['basket_quantity']
 
--- basket/views.py
+++ basket/views.py
@@ -14,19 +14,52 @@ def basket_add(request):
     basket = Basket(request)
     
     if request.POST.get('action') == 'post':
-        
         product_id = int(request.POST.get('productID'))
         product_quantity = int(request.POST.get('productQuantity'))
 
         product = get_object_or_404(Product, pk=product_id)
         basket.add(product=product, product_quantity=product_quantity)
 
-        response = JsonResponse({
-            'quantity': len(basket)
+        return JsonResponse({
+            'basket_quantity': basket.get_basket_quantity,
+            'basket_price': basket.get_basket_price,
+            'item_total_price': basket.basket[str(product_id)]['total_price'],
         })
 
-        return response
+    return JsonResponse({'success': 'failed'})
 
-    return JsonResponse({
-            'test': 'failed'
+
+def basket_delete(request):
+    basket = Basket(request)
+
+    if request.POST.get('action') == 'delete':
+        
+        product_id = int(request.POST.get('productID'))
+
+        product = get_object_or_404(Product, pk=product_id)
+        basket.delete(product=product)
+
+        return JsonResponse({
+            'basket_quantity': basket.get_basket_quantity,
+            'basket_price': basket.get_basket_price,
         })
+
+    return JsonResponse({'success': 'failed'})
+
+
+# def basket_add(request):
+#     basket = Basket(request)
+    
+#     if request.POST.get('action') == 'post':
+        
+#         product_id = int(request.POST.get('productID'))
+#         product_quantity = int(request.POST.get('productQuantity'))
+
+#         product = get_object_or_404(Product, pk=product_id)
+#         basket.add(product=product, product_quantity=product_quantity)
+
+#         return JsonResponse({
+#             'basket_quantity': basket.get_basket_quantity,
+#         })
+
+#     return JsonResponse({'success': 'failed'})
