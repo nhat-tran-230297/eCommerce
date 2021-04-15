f92c42bd98575d5360fd3c37c640fd94d635fe65
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-04-16 02:22:09 +0300

    test models

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,14 +1,14 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
+from account.models import UserBase
 from store.models import Category, Product
 
 
 # Create your tests here.
 class TestCategoryModel(TestCase):
     def setUp(self):
-        self.data1 = Category.objects.create(name='django', slug='django')
+        self.data1 = Category.objects.create(name="django", slug="django")
 
     def test_category_model_entry(self):
         """
@@ -16,7 +16,7 @@ class TestCategoryModel(TestCase):
         """
         data = self.data1
         self.assertTrue(isinstance(data, Category))
-        self.assertEqual(str(data), 'django')
+        self.assertEqual(str(data), "django")
 
     # def test_category_url(self):
     #     """
@@ -30,23 +30,24 @@ class TestCategoryModel(TestCase):
 
 class TestProductModel(TestCase):
     def setUp(self):
-        Category.objects.create(name='django', slug='django')
-        User.objects.create(username='aimblaster')
+        Category.objects.create(name="django", slug="django")
+        UserBase.objects.create(username="aimblaster")
         self.data1 = Product.objects.create(
-                category_id=1, 
-                title='django beginners', 
-                product_creator_id=1,
-                slug='django-beginners', 
-                price='20.00', 
-                image='django')
+            category_id=1,
+            title="django beginners",
+            product_creator_id=1,
+            slug="django-beginners",
+            price="20.00",
+            image="django",
+        )
 
         # self.data2 = Product.products.create(
-        #         category_id=1, 
-        #         title='django advanced', 
+        #         category_id=1,
+        #         title='django advanced',
         #         product_creator_id=1,
-        #         slug='django-advanced', 
-        #         price='20.00', 
-        #         image='django', 
+        #         slug='django-advanced',
+        #         price='20.00',
+        #         image='django',
         #         is_active=False)
 
     def test_product_model_entry(self):
@@ -55,6 +56,4 @@ class TestProductModel(TestCase):
         """
         data = self.data1
         self.assertTrue(isinstance(data, Product))
-        self.assertEqual(str(data), 'django beginners')
-
-       
+        self.assertEqual(str(data), "django beginners")
