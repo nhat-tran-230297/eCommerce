aebb8b21c99e6c7df4a0036ab1bec5e36c658fae
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-04-16 02:24:24 +0300

    add comments and notation

--- store/views.py
+++ store/views.py
@@ -6,28 +6,37 @@ from .models import Category, Product
 
 
 def product_all(request):
+    """
+    Home page - display all active products
+    """
+
     products = Product.active_products.all()
 
-    context = {
-        'products': products
-    }
-    return render(request, 'store/home.html', context)
+    context = {"products": products}
+    return render(request, "store/home.html", context)
 
 
-def product_detail(request, slug):
+def product_detail(request, slug:str):
+    """
+    Product detail view
+    """
+
     product = get_object_or_404(Product, slug=slug, is_instock=True)
 
-    context = {
-        'product': product
-    }
-    return render(request, 'store/product/detail.html', context)
+    context = {"product": product}
+    return render(request, "store/product/detail.html", context)
 
-def category_list(request, category_slug):
+
+def category_list(request, category_slug:str):
+    """
+    Category list view (lists of products regarding the category)
+    """
+
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
 
     context = {
-        'category': category,
-        'products': products,
+        "category": category,
+        "products": products,
     }
-    return render(request, 'store/product/category.html', context)
+    return render(request, "store/product/category.html", context)
