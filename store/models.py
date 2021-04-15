7eaea9844876e2870150465dba0d289067415c59
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-04-16 02:20:26 +0300

    add unique constraint to slug field

--- store/models.py
+++ store/models.py
@@ -1,43 +1,60 @@
 # from django.contrib.auth.models import User
+from account.models import UserBase
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
 
 class ProductActiveManager(models.Manager):
+    """
+    Custom Manager for products, where is_active=True
+    """
+
     def get_queryset(self):
         return super().get_queryset().filter(is_active=True)
 
 
 class ProductInstockManager(models.Manager):
+    """
+    Custom Manager for products, where is_instock=True
+    """
+
     def get_queryset(self):
         return super().get_queryset().filter(is_instock=True)
 
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=255, db_index=True)
+    """
+    Category Model
+    """
+
+    name = models.CharField(max_length=255, db_index=True, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
 
     class Meta:
-        # as default, on the admin page is 'Categorys' -> overwrite to 'categories'
-        verbose_name_plural = 'categories'
+        # as default, on the admin page is 'Categorys' -> overwrite to 'Categories'
+        verbose_name_plural = "categories"
 
     def get_absolute_url(self):
-        return reverse('store:category_list', kwargs={'category_slug': self.slug})
+        return reverse("store:category_list", kwargs={"category_slug": self.slug})
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE) 
-    product_creator = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='products', on_delete=models.CASCADE)
+    """
+    Product Model
+    """
+
+    category = models.ForeignKey(Category, related_name="product", on_delete=models.PROTECT)
+    product_creator = models.ForeignKey(UserBase, related_name="products", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255, default='admin')
+    author = models.CharField(max_length=255, default="admin")
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='images/default.png')
-    slug = models.SlugField(max_length=255)
+    image = models.ImageField(upload_to="images/", default="images/default.png")
+    slug = models.SlugField(max_length=255, unique=True)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     is_instock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
@@ -47,17 +64,19 @@ class Product(models.Model):
     updated_time = models.DateTimeField(auto_now=True)
 
     # custom Manager #
-    objects = models.Manager()      # The default manager.
-    active_products = ProductActiveManager() # active_products specific manager
+    objects = models.Manager()  # The default manager.
+    active_products = ProductActiveManager()  # active_products specific manager
     instock_products = ProductInstockManager()  # instock_products specific manager
-
 
     class Meta:
         # order of products: latest -> oldest
-        ordering = ('-created_time',)
+        ordering = ("-created_time",)
 
     def get_absolute_url(self):
-        return reverse('store:product_detail', kwargs={'slug': self.slug})
+        """
+        url of detail-view of each product
+        """
+        return reverse("store:product_detail", kwargs={"slug": self.slug})
 
     def __str__(self):
         return self.title
