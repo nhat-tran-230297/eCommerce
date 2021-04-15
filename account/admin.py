7ae968f9bcdba38c5f0fcc0a496fabedcfdaef00
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-04-16 02:06:32 +0300

    override admin for adding UserBase

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
+from .forms import AdminRegistrationForm
 from .models import UserBase
 
+
 # Register your models here.
-admin.site.register(UserBase)
+@admin.register(UserBase)
+class UserBaseAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username",)}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions"),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+
+    add_form = AdminRegistrationForm
