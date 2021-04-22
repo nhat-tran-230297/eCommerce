6d9b1c8447e2f318e0426cd279f6fc5b45ed8b5e
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-04-23 00:15:33 +0300

    add notation and comments

--- account/models.py
+++ account/models.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin)
 from django.core.mail import send_mail
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
+from rest_framework.authtoken.models import Token
 
 
 class CustomAccountManager(BaseUserManager):
@@ -74,3 +78,9 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+# generate tokens after user registration
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
