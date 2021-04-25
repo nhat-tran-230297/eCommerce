38a717cca7e5a729f0d2dacff06f880b236275e9
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-04-25 21:55:26 +0300

    add product context processor

--- store/context_processors.py
+++ store/context_processors.py
@@ -1,11 +1,16 @@
 from django.conf import settings
 
-from .models import Category
+from .models import Category, Product
 
 
 def categories(request) -> dict:
     return { 
         'categories_context_processor': Category.objects.all() 
+    }
+
+def products(request) -> dict:
+    return { 
+        'products_context_processor': Product.active_products.all() 
     }
 
 
