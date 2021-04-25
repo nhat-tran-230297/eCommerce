e941a0337c7b7db3fd3092a30e5f8da15e7c78c6
nhat-tran-230297 <ilovenhat230297@gmail.com>
2021-04-25 21:55:58 +0300

    add product search field

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.product_all, name='product_all'),
-    path('<slug:slug>', views.product_detail, name='product_detail'),
-    path('shop/<slug:category_slug>', views.category_list, name='category_list')
+    path('', views.HomeView.as_view(), name='home_view'),
+    path('product/<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
+    path('category/<slug:category_slug>', views.CategoryProductListView.as_view(), name='category_product_list'),
+    path('search/', views.ProductSearchResultsView.as_view(), name='product_search_results'),
 ]
